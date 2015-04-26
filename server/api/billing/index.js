ce79919630c59a984ed520a6045707b91feca4a2
Adam Wamai Egesa <adam.wamai@gmail.com>
2015-04-27 00:03:58 +0200

    Changed the authorisation level of some API methods for billing.

--- server/api/billing/index.js
+++ server/api/billing/index.js
@@ -5,11 +5,11 @@ var controller = require('./billing.controller');
 
 var router = express.Router();
 
-router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.isAuthenticated(), controller.update);
-router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
