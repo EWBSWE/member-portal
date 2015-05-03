8c05d323820ad9909194506460ca8ab97202798c
Adam Wamai Egesa <adam.wamai@gmail.com>
2015-05-03 14:00:06 +0200

    New API endpoints for admin and restricted other non-admin endpoints

--- server/api/billing/billing.controller.js
+++ server/api/billing/billing.controller.js
@@ -11,39 +11,75 @@ exports.index = function(req, res) {
   });
 };
 
-// Get a single billing
-exports.show = function(req, res) {
+// Get a users single billing
+exports.getMyBilling = function(req, res) {
+  var userId = req.user._id;
+  return getBilling(userId, res);
+};
+
+// Get a single billing (given Admin Access Control)
+exports.getUsersBilling = function(req, res) {
+  var userId = req.params.user;
+  return getBilling(userId, res);
+};
+
+function getBilling(userId, res){
   Billing.findById(req.params.id, function (err, billing) {
     if(err) { return handleError(res, err); }
     if(!billing) { return res.sendStatus(404); }
+    if(billing.user !== userId){ return res.sendStatus(403);}
     return res.json(billing);
   });
-};
+}
 
 // Get a single users billings
 exports.getUsersBillings = function(req, res) {
+  var userId = req.params.user;
+  getBillings(userId, res);
+};
+
+// Get the users' billings
+exports.getMyBillings = function(req, res) {
   var userId = req.user._id;
+  getBillings(userId, res);
+};
+
+function getBillings(userId, res){
   Billing.find({user: userId}, function (err, billings) {
     if(err) { return handleError(res, err); }
     if(!billings) { return res.sendStatus(404); }
     return res.json(billings);
   });
+}
+
+// Creates a new billing in the DB.
+exports.createUsersBilling = function(req, res) {
+  var userId = req.params.user;
+  return createBilling(userId, req, res);
 };
 
 // Creates a new billing in the DB.
-exports.create = function(req, res) {
+exports.createMyBilling = function(req, res) {
+  var userId = req.user._id;
+  return createBilling(userId, req, res);
+};
+
+function createBilling(userId, req, res){
+  if(req.body.user !== userId){ return res.sendStatus(403);}
   Billing.create(req.body, function(err, billing) {
     if(err) { return handleError(res, err); }
     return res.status(201).json(billing);
   });
-};
+}
 
 // Updates an existing billing in the DB.
 exports.update = function(req, res) {
+  var userId = req.user._id;
   if(req.body._id) { delete req.body._id; }
   Billing.findById(req.params.id, function (err, billing) {
     if (err) { return handleError(res, err); }
     if(!billing) { return res.sendStatus(404); }
+    if(billing.user !== userId){ return res.sendStatus(403);}
     var updated = _.merge(billing, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
--- server/api/billing/index.js
+++ server/api/billing/index.js
@@ -7,11 +7,17 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
-router.get('/my', auth.isAuthenticated(), controller.getUsersBillings);
-router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
+
+router.get('/user/:user', auth.hasRole('admin'), controller.getUsersBillings);
+router.get('/my', auth.isAuthenticated(), controller.getMyBillings);
+
+router.get('/:id/user/:user', auth.hasRole('admin'), controller.getUsersBilling);
+router.get('/:id', auth.isAuthenticated(), controller.getMyBilling);
+
+router.post('/user/:user', auth.hasRole('admin'), controller.createUsersBilling);
+router.post('/', auth.isAuthenticated(), controller.createMyBilling);
+
 router.put('/:id', auth.isAuthenticated(), controller.update);
-router.patch('/:id', auth.hasRole('admin'), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
