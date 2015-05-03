4f5fad77a72661ea7abb6e3c2658c8f65f3ed17b
Adam Wamai Egesa <adam.wamai@gmail.com>
2015-05-03 13:03:13 +0200

    Added API endpoint for collecting a users billings.

--- server/api/billing/billing.controller.js
+++ server/api/billing/billing.controller.js
@@ -7,7 +7,7 @@ var Billing = require('./billing.model');
 exports.index = function(req, res) {
   Billing.find(function (err, billings) {
     if(err) { return handleError(res, err); }
-    return res.json(200, billings);
+    return res.status(200).json(billings);
   });
 };
 
@@ -15,8 +15,18 @@ exports.index = function(req, res) {
 exports.show = function(req, res) {
   Billing.findById(req.params.id, function (err, billing) {
     if(err) { return handleError(res, err); }
-    if(!billing) { return res.send(404); }
+    if(!billing) { return res.sendStatus(404); }
     return res.json(billing);
+  });
+};
+
+// Get a single users billings
+exports.getUsersBillings = function(req, res) {
+  var userId = req.user._id;
+  Billing.find({user: userId}, function (err, billings) {
+    if(err) { return handleError(res, err); }
+    if(!billings) { return res.sendStatus(404); }
+    return res.json(billings);
   });
 };
 
@@ -24,7 +34,7 @@ exports.show = function(req, res) {
 exports.create = function(req, res) {
   Billing.create(req.body, function(err, billing) {
     if(err) { return handleError(res, err); }
-    return res.json(201, billing);
+    return res.status(201).json(billing);
   });
 };
 
@@ -33,11 +43,11 @@ exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
   Billing.findById(req.params.id, function (err, billing) {
     if (err) { return handleError(res, err); }
-    if(!billing) { return res.send(404); }
+    if(!billing) { return res.sendStatus(404); }
     var updated = _.merge(billing, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
-      return res.json(200, billing);
+      return res.status(200).json(billing);
     });
   });
 };
@@ -49,11 +59,11 @@ exports.destroy = function(req, res) {
     if(!billing) { return res.send(404); }
     billing.remove(function(err) {
       if(err) { return handleError(res, err); }
-      return res.send(204);
+      return res.sendStatus(204);
     });
   });
 };
 
 function handleError(res, err) {
-  return res.send(500, err);
+  return res.status(500).send(err);
 }
--- server/api/billing/index.js
+++ server/api/billing/index.js
@@ -7,6 +7,7 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/my', auth.isAuthenticated(), controller.getUsersBillings);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
