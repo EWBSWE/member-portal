6a33f5e788e891f4081ac27ce5b8c375b87dd804
Dan Johansson <dan.albin.johansson@gmail.com>
2015-07-23 00:14:00 +0200

    add client actions for removing users

--- client/app/member/list/list.controller.js
+++ client/app/member/list/list.controller.js
@@ -2,9 +2,19 @@
 
 angular.module('ewbMemberApp')
   .controller('MemberListCtrl', function ($scope, $http) {
-    $scope.members= [];
+    $scope.members = [];
 
     $http.get('/api/members').success(function(members) {
       $scope.members = members;
     });
+
+    $scope.deleteUser = function(id) {
+      if (confirm('Are you sure you want to remove user? This action cannot be undone.')) {
+        $http.delete('/api/members/' + id).success(function(member) {
+          $http.get('/api/members').success(function(members) {
+            $scope.members = members;
+          });
+        });
+      }
+    };
   });
