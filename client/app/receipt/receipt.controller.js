034c0f7bd903fc55037cc2714817b9e0dc4e53fb
Dan Johansson <dan.albin.johansson@gmail.com>
2015-08-01 12:47:33 +0200

    update receipt view

--- client/app/receipt/receipt.controller.js
+++ client/app/receipt/receipt.controller.js
@@ -3,10 +3,24 @@
 angular.module('ewbMemberApp')
   .controller('ReceiptCtrl', function ($scope, $http, $location) {
     $scope.receipt = {};
+    $scope.member = {};
 
-    $http.get('/api/payments/' + $location.search().id).success(function(data, status) {
-      $scope.receipt = data;
+    $http.get('/api/payments/' + $location.search().id).success(function(receipt, status) {
+      $scope.receipt = receipt;
+      $http.get('/api/members/' + receipt.member).success(function(member, status) {
+        $scope.member = member;
+      }).error(function(data, status) {
+        console.log('Misslyckades med att hamta medlem');
+      });
     }).error(function(data, status) {
-      console.log('misslyckades med att hamta kvitto');
+      console.log('Misslyckades med att hamta kvitto');
     });
+
+    $scope.memberTypeText = function() {
+      var text = 'Medlem som yrkesverksam/senior';
+      if ($scope.member.student) {
+        text = 'Studentmedlem';
+      }
+      return text;
+    };
   });
