1e69485e205f0e03d96f8885319755f56c190176
Dan Johansson <dan.albin.johansson@gmail.com>
2015-08-17 21:05:57 +0200

    Require authentication on member

--- client/app/member/member.js
+++ client/app/member/member.js
@@ -9,18 +9,22 @@ angular.module('ewbMemberApp')
       })
       .when('/members', {
         templateUrl: 'app/member/list/list.html',
-        controller: 'MemberListCtrl'
+        controller: 'MemberListCtrl',
+        authenticate: true, 
       })
       .when('/member/bulk', {
         templateUrl: 'app/member/bulk/bulk.html',
-        controller: 'MemberBulkCtrl'
+        controller: 'MemberBulkCtrl',
+        authenticate: true, 
       })
       .when('/member/new', {
         templateUrl: 'app/member/new/new.html',
-        controller: 'MemberNewCtrl'
+        controller: 'MemberNewCtrl',
+        authenticate: true, 
       })
       .when('/member/:id', {
         templateUrl: 'app/member/detail/detail.html',
-        controller: 'MemberDetailCtrl'
+        controller: 'MemberDetailCtrl',
+        authenticate: true, 
       });
   });
