f8b4dad5cc1ece65e58818e6668120d15c4c1427
Dan Johansson <dan.albin.johansson@gmail.com>
2015-08-17 20:48:27 +0200

    Add admin requirement on routes

--- server/api/member/index.js
+++ server/api/member/index.js
@@ -2,15 +2,16 @@
 
 var express = require('express');
 var controller = require('./member.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.hasRole('admin'), controller.index);
+router.get('/:id', auth.hasRole('admin'), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
-router.post('/bulk', controller.bulkAdd);
-router.get('/:id/payments', controller.getPayments);
+router.post('/bulk', auth.hasRole('admin'), controller.bulkAdd);
+router.get('/:id/payments', auth.hasRole('admin'), controller.getPayments);
 
 module.exports = router;
