005a8a72a3bd08bfbea42f03466cd3ef6ed07cc1
Dan Johansson <dan.albin.johansson@gmail.com>
2015-09-10 23:43:30 +0200

    Remove header

--- client/components/header/header.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ewbMemberApp')
-  .controller('HeaderCtrl', function ($scope, $location) {
-    $scope.navbarPath = 'components/navbar/navbar.html';
-    if (/bli-medlem/.test($location.path()) || /kvitto/.test($location.path())) {
-      $scope.navbarPath = 'components/public-navbar/public-navbar.html';
-    }
-  });
