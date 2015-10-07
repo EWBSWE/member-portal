26874b6a17448278f4c955b1b9bdbcc19bf1e2bf
Dan Johansson <dan.albin.johansson@gmail.com>
2015-10-08 00:26:25 +0200

    add 'add user' helper script

---
+++ helper/add-user.js
@@ -0,0 +1,30 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var mongoose = require('mongoose');
+var path = require('path');
+var config = require(path.join(__dirname, '../server/config/environment'));
+var User = require(path.join(__dirname, '../server/models/user.model'));
+
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+console.log(process.argv);
+
+var email = process.argv[2];
+var password = process.argv[3];
+var role = process.argv[4];
+
+if (!email && !password) {
+    console.log('Usage: node add-user.js <email> <password> <role>');
+    process.exit(1);
+}
+
+User.create({ email: email, password: password, role: role }, function(err, data) {
+    if (err) {
+        console.log(err);
+        process.exit(1)
+    }
+    console.log(data);
+    process.exit(0);
+});
+
+
