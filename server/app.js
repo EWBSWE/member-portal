4335dd5596a9b6998c603255ee995f2d0d07ddbb
Dan Johansson <dan.albin.johansson@gmail.com>
2015-10-07 19:29:11 +0200

    set developmail

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 
-process.env.DEV_MAIL = process.env.DEV_MAIL || config.developerMail();
+process.env.DEV_MAIL = process.env.DEV_MAIL || config.developerMail;
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
