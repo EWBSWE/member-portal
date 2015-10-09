a75d4bbcccd7e1339ec9b12255f05bf13250b37d
Dan Johansson <dan.albin.johansson@gmail.com>
2015-10-09 23:59:11 +0200

    sort errors

--- client/app/ewb-error/ewb-error.controller.js
+++ client/app/ewb-error/ewb-error.controller.js
@@ -3,6 +3,8 @@
 angular.module('ewbMemberApp')
   .controller('EWBErrorCtrl', function ($scope, $http, Auth, User) {
     $scope.ewbErrors = [];
+    $scope.sortBy = 'createdAt';
+    $scope.sortReverse = false;
 
     $http.get('/api/errors').success(function(data) {
       console.log(data);
