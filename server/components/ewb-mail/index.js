65fa9974a6df029b5fe10116f4f48eccf4ffd63a
Dan Johansson <dan.albin.johansson@gmail.com>
2015-11-22 21:16:54 +0100

    update ewb mail sender

--- server/components/ewb-mail/index.js
+++ server/components/ewb-mail/index.js
@@ -7,7 +7,7 @@ module.exports = {
     sender: function() {
         // Should perhaps be set from a config file instead of directly in the
         // code like this.
-        return 'volontar@ingenjorerutangranser.se';
+        return 'Ingenjörer utan gränser <volontar@ingenjorerutangranser.se>';
     },
     getSubject: function(name) {
         return getContent(name + '-subject.txt');
