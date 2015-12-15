f9dedd23f51b0a95084512c1bdbbce533deb83f8
Adam Wamai Egesa <adam.wamai@gmail.com>
2015-12-15 02:42:20 +0100

    test(user): commented deprecated fields and unit tests

--- server/models/user.model.spec.js
+++ server/models/user.model.spec.js
@@ -7,8 +7,8 @@ var User = require('./user.model');
 var user = new User({});
 var initUser = function() {
   user.provider = 'local';
-  user.firstName = 'Fake';
-  user.lastName = 'User';
+  /*user.firstName = 'Fake';
+  user.lastName = 'User';*/
   user.email = 'test@test.com';
   user.password = 'password';
 };
@@ -54,7 +54,7 @@ describe('User Model', function() {
     });
   });
 
-  it('should fail when saving without a firstName', function(done) {
+ /* it('should fail when saving without a firstName', function(done) {
     user.firstName = '';
     user.save(function(err) {
       should.exist(err);
@@ -68,7 +68,7 @@ describe('User Model', function() {
       should.exist(err);
       done();
     });
-  });
+  });*/
 
   it("should authenticate user if password is valid", function() {
     return user.authenticate('password').should.be.true;
