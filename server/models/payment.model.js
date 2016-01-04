b36cc8bc757a389d91aaba022746111a4fdff981
Dan Johansson <dan.albin.johansson@gmail.com>
2016-01-05 00:27:11 +0100

    change default new date to moment

--- server/models/payment.model.js
+++ server/models/payment.model.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var moment = require('moment');
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
@@ -7,7 +8,7 @@ var PaymentSchema = new Schema({
   member: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true },
   amount: { type: Number, required: true },
   currency: { type: String, required: true, default: 'SEK' },
-  createdAt: { type: Date, required: true, default: new Date() },
+  createdAt: { type: Date, required: true, default: moment() },
 });
 
 PaymentSchema.index({member: 1});
