f70108389ae0e7e6496a6b315c6ed3f88f005521
Dan Johansson <dan.albin.johansson@gmail.com>
2016-01-05 16:55:20 +0100

    add cron job for ewberrors

---
+++ cron/ewb-error.js
@@ -0,0 +1,61 @@
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var mongoose = require('mongoose');
+var path = require('path');
+var config = require(path.join(__dirname, '../server/config/environment'));
+
+var OutgoingMessage = require(path.join(__dirname, '../server/models/outgoing-message.model'));
+var ewbError = require(path.join(__dirname, '../server/models/ewb-error.model'));
+
+var moment = require('moment');
+
+var mailgun;
+if (process.env.NODE_ENV === 'production') {
+    mailgun = require('mailgun-js')({
+        apiKey: '***REMOVED***',
+        domain: 'blimedlem.ingenjorerutangranser.se',
+    });
+} else {
+    mailgun = require('mailgun-js')({
+        apiKey: '***REMOVED***',
+        domain: '***REMOVED***',
+    });
+}
+
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+ewbError.find({ createdAt: { $gt: moment().subtract(1, 'day') } }, function(err, ewbErrors) {
+    if (err) {
+        ewbError.create({ message: 'Fetch ewb errors', origin: __filename, params: err }, function (err, data) {
+            // Exit with failure code
+            process.exit(1);
+        });
+    }
+
+    if (ewbErrors.length) {
+        var data = {
+            from: 'ict@ingenjorerutangranser.se',
+            to: 'ict@ingenjorerutangranser.se',
+            subject: 'ewb-member: ' + ewbErrors.length + ' errors',
+            text: 'Since: ' + moment().subtract(1, 'day').format(),
+        };
+
+        OutgoingMessage.create(data, function() {
+            var err = arguments[0];
+            if (err) {
+                process.exit(1);
+            }
+
+            for (var i = 1; i < arguments.length; i++) {
+                var message = arguments[i];
+                console.log(message);
+            }
+
+            process.exit(0);
+        });
+    } else {
+        process.exit(0);
+    }
+});
