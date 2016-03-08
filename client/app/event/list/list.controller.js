8eea2c71b6140b2da2c855c275bcd3f07be9439e
Dan Johansson <dan.albin.johansson@gmail.com>
2016-03-08 08:44:32 +0100

    fix event list view

--- client/app/event/list/list.controller.js
+++ client/app/event/list/list.controller.js
@@ -2,11 +2,20 @@
 /* globals confirm*/
 
 angular.module('ewbMemberApp')
-  .controller('EventListCtrl', function ($scope, $http) {
+.controller('EventListCtrl', function ($scope, $http) {
     $scope.events = [];
 
     $http.get('/api/events').success(function(events) {
-      $scope.events = events;
+        $scope.events = events;
     });
 
-  });
+    $scope.deleteEvent = function(id) {
+        if (confirm('Are you sure you want to remove event? This action cannot be undone.')) {
+            $http.delete('/api/events/' + id).success(function(result) {
+                $http.get('/api/events').success(function(events) {
+                    $scope.events = events;
+                });
+            });
+        }
+    };
+});
