c2b7ad234818f3360615ac5abdd40dd86339f021
Dan Johansson <dan.albin.johansson@gmail.com>
2016-03-14 21:59:10 +0100

    remove debug text

--- client/app/ewb-error/ewb-error.controller.js
+++ client/app/ewb-error/ewb-error.controller.js
@@ -1,13 +1,12 @@
 'use strict';
 
 angular.module('ewbMemberApp')
-  .controller('EWBErrorCtrl', function ($scope, $http, Auth, User) {
+.controller('EWBErrorCtrl', function ($scope, $http, Auth, User) {
     $scope.ewbErrors = [];
     $scope.sortBy = 'createdAt';
     $scope.sortReverse = false;
 
     $http.get('/api/errors').success(function(data) {
-      console.log(data);
-      $scope.ewbErrors = data;
+        $scope.ewbErrors = data;
     });
-  });
+});
