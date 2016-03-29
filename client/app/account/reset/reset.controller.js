b4dea8b6fcbdf4dacb536116f9232303cb838c58
Dan Johansson <dan.albin.johansson@gmail.com>
2016-03-29 11:05:29 +0200

    redirect to login after password reset

--- client/app/account/reset/reset.controller.js
+++ client/app/account/reset/reset.controller.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('ewbMemberApp')
-  .controller('ResetCtrl', function ($scope, $location, $http) {
+.controller('ResetCtrl', function ($scope, $location, $http) {
     $scope.reset = function (form) {
-      if (form.$valid && $location.search().token) {
-        $http.post('/api/users/reset-password-token', { 
-          newPassword: $scope.password,
-          token: $location.search().token,
-        }).success(function (data) {
-          $location.path('/');
-        });
-      }
+        if (form.$valid && $location.search().token) {
+            $http.post('/api/users/reset-password-token', { 
+                newPassword: $scope.password,
+                token: $location.search().token,
+            }).success(function (data) {
+                $location.path('/login');
+            });
+        }
     };
-  });
+});
