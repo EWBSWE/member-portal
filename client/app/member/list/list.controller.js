5e9daad1d2a96e64b73dcbd45bc6cdefbf516d41
Dan Johansson <dan.albin.johansson@gmail.com>
2017-05-02 20:35:28 +0200

    filter location if member location is null

--- client/app/member/list/list.controller.js
+++ client/app/member/list/list.controller.js
@@ -2,82 +2,86 @@
 /* globals confirm*/
 
 angular.module('ewbMemberApp')
-  .controller('MemberListCtrl', function ($scope, $http) {
-    $scope.members = [];
-    $scope.sortBy = 'name';
-    $scope.sortReverse = false;
+    .controller('MemberListCtrl', function ($scope, $http) {
+        $scope.members = [];
+        $scope.sortBy = 'name';
+        $scope.sortReverse = false;
 
-    $http.get('/api/members').success(function(members) {
-      $scope.members = members;
-    });
-
-    $scope.deleteUser = function(id) {
-      if (confirm('Are you sure you want to remove user? This action cannot be undone.')) {
-        $http.delete('/api/members/' + id).success(function(member) {
-          $http.get('/api/members').success(function(members) {
+        $http.get('/api/members').success(function(members) {
             $scope.members = members;
-          });
-        });
-      }
-    };
-
-    $scope.generateCsv = function() {
-      if ($scope.members.length === 0) {
-        return;
-      }
-
-      var membersToExport = $scope.members;
-      if ($scope.emailFilter || $scope.locationFilter) {
-        var tmp = [];
-
-        _.each($scope.members, function (member) {
-          if ($scope.emailFilter && $scope.filterByEmail(member)) {
-            tmp.push(member);
-          } else if ($scope.locationFilter && $scope.filterByLocation(member)) {
-            tmp.push(member);
-          }
         });
 
-        membersToExport = tmp;
-      }
+        $scope.deleteUser = function(id) {
+            if (confirm('Are you sure you want to remove user? This action cannot be undone.')) {
+                $http.delete('/api/members/' + id).success(function(member) {
+                    $http.get('/api/members').success(function(members) {
+                        $scope.members = members;
+                    });
+                });
+            }
+        };
 
-      var keysToIgnore = ['id'];
-      var csvContent = 'data:text/csv;charset=utf-8,';
+        $scope.generateCsv = function() {
+            if ($scope.members.length === 0) {
+                return;
+            }
 
-      var memberKeys = Object.keys($scope.members[0]);
-      _.remove(memberKeys, function(key) {
-        return _.contains(keysToIgnore, key);
-      });
+            var membersToExport = $scope.members;
+            if ($scope.emailFilter || $scope.locationFilter) {
+                var tmp = [];
 
-      // Create headers of the member keys
-      csvContent += memberKeys.join(',') + '\n';
+                _.each($scope.members, function (member) {
+                    if ($scope.emailFilter && $scope.filterByEmail(member)) {
+                        tmp.push(member);
+                    } else if ($scope.locationFilter && $scope.filterByLocation(member)) {
+                        tmp.push(member);
+                    }
+                });
 
-      _.each(membersToExport, function(member, index) {
-        csvContent += _.map(memberKeys, function(key) {
-          return member[key];
-        }).join(',');
+                membersToExport = tmp;
+            }
 
-        if (index < $scope.members.length) {
-          csvContent += '\n';
-        }
-      });
+            var keysToIgnore = ['id'];
+            var csvContent = 'data:text/csv;charset=utf-8,';
 
-      window.open(encodeURI(csvContent));
-    };
+            var memberKeys = Object.keys($scope.members[0]);
+            _.remove(memberKeys, function(key) {
+                return _.contains(keysToIgnore, key);
+            });
 
-    $scope.filterByEmail = function(member) {
-      if (!$scope.emailFilter) {
-        return true;
-      }
+            // Create headers of the member keys
+            csvContent += memberKeys.join(',') + '\n';
 
-      return member.email.match(new RegExp($scope.emailFilter, 'i'));
-    };
+            _.each(membersToExport, function(member, index) {
+                csvContent += _.map(memberKeys, function(key) {
+                    return member[key];
+                }).join(',');
 
-    $scope.filterByLocation = function (member) {
-      if (!$scope.locationFilter) {
-        return true;
-      }
+                if (index < $scope.members.length) {
+                    csvContent += '\n';
+                }
+            });
 
-      return member.location.match(new RegExp($scope.locationFilter, 'i'));
-    };
-  });
+            window.open(encodeURI(csvContent));
+        };
+
+        $scope.filterByEmail = function(member) {
+            if (!$scope.emailFilter) {
+                return true;
+            }
+
+            return member.email.match(new RegExp($scope.emailFilter, 'i'));
+        };
+
+        $scope.filterByLocation = function (member) {
+            if (!$scope.locationFilter) {
+                return true;
+            }
+
+            if (!member.location) {
+                return false;
+            }
+
+            return member.location.match(new RegExp($scope.locationFilter, 'i'));
+        };
+    });
