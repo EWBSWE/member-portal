9ea30d2f9433791da69ef1ab3b57e9499fea96f7
Dan Johansson <dan.albin.johansson@gmail.com>
2018-10-08 20:37:32 +0200

    Add product repository

---
+++ server/api/product/ProductRepository.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const db = require('../../db').db;
+
+const { MembershipProduct } = require('./Product');
+
+class ProductRepository {
+  async getMembership(id) {
+    const entity = await db.one(`
+	SELECT *
+	FROM product
+	WHERE id = $1
+	    AND product_type_id = (
+		SELECT id
+		FROM product_type
+		WHERE identifier = 'membership'
+	    )
+    `, [id]);
+
+    return this._toMembershipProduct(entity);
+  }
+
+  _toMembershipProduct(entity) {
+    return new MembershipProduct(
+      entity.id,
+      entity.name,
+      entity.price,
+      entity.description,
+      entity.product_type_id,
+      entity.attribute.member_type_id,
+      entity.attribute.days
+    );
+  }
+}
+
+module.exports = new ProductRepository();
