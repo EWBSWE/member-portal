c60414d4b7f73c15fe292b4a8422c77b2f14c7f1
Dan Johansson <dan.albin.johansson@gmail.com>
2020-05-18 17:09:31 +0200

    Move PgUserStore to its own file

---
+++ server/user/PgUserStore.ts
@@ -0,0 +1,17 @@
+import { UserEntity } from './UserEntity';
+import { db } from '../db';
+import { SqlProvider } from "../SqlProvider"
+import { UserStore } from './UserStore';
+
+export class PgUserStore implements UserStore {
+    readonly sqlProvider: SqlProvider
+
+    constructor(sqlProvider: SqlProvider) {
+        this.sqlProvider = sqlProvider;
+    }
+
+    async create(entity: UserEntity): Promise<UserEntity> {
+        const result = await db.one(this.sqlProvider.insertUser, [entity.username, entity.hashedPassword, entity.salt, entity.role]);
+        return new UserEntity(result[0], result[1], result[2], result[3]);
+    }
+}
