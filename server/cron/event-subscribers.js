75a5797b05c9530ddcb6e991ebf285eb599ede58
Dan Johansson <dan.albin.johansson@gmail.com>
2020-05-26 15:22:19 +0200

    Update cron job for subscribers

--- server/cron/event-subscribers.js
+++ server/cron/event-subscribers.js
@@ -1,90 +1,103 @@
-'use strict';
+"use strict";
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const path = require('path');
-const moment = require('moment');
+const path = require("path");
+const moment = require("moment");
 
-require('dotenv')
-    .config({ path: path.resolve(__dirname, '../../env') });
+require("dotenv").config({ path: path.resolve(__dirname, "../../env") });
 
-const ewbMail = require(path.join(__dirname, '../components/ewb-mail'));
-const log = require(path.join(__dirname, '../config/logger'));
-const db = require(path.join(__dirname, '../db')).db;
+const ewbMail = require(path.join(__dirname, "../components/ewb-mail"));
+const log = require(path.join(__dirname, "../config/logger"));
+const db = require(path.join(__dirname, "../db")).db;
 
-const Event = require(path.join(__dirname, '../models/event.model'));
+const { EventRepository } = require(path.join(
+  __dirname,
+  "../event/EventRepository"
+));
+const { SqlProvider } = require(path.join(__dirname, "../SqlProvider"));
+const eventRepository = new EventRepository(db, SqlProvider);
 
-db.any(`
+db.any(
+  `
     SELECT id
     FROM event
     WHERE event.active
-`).then(activeEvents => {
+`
+)
+  .then((activeEvents) => {
     if (activeEvents.length === 0) {
-        log.info('No active events');
-        process.exit(0);
+      log.info("No active events");
+      process.exit(0);
     }
 
-    let events = activeEvents.map(e => {
-        return Event.get(e.id);
-    });
-
-    return Promise.all(events);
-}).then(events => {
+    return eventRepository.findAll();
+  })
+  .then((events) => {
     let mails = [];
 
-    events.forEach(e => {
-        if (e.subscribers.length === 0) {
-            log.info('No subscribers for ' + e.identifier);
-            return;
-        }
+    events.forEach((e) => {
+      if (e.subscribers.length === 0) {
+        log.info("No subscribers for " + e.identifier);
+        return;
+      }
 
-        let eventSummary = 'Inga anmälningar';
-        if (e.payments.length > 0) {
-            eventSummary = 'Namn | Epost | Betalt (SEK) | Val | Meddelande\n\n';
-            eventSummary += e.payments.map(p => {
-                let products = e.addons.filter(a => {
-                    return p.addons.includes(a.product_id);
-                });
-
-                let productNames = products.map(product => {
-                    return product.name;
-                }).join(', ');
-
-                let message = p.message === null ? "-" : p.message;
-
-                return `${p.name} | ${p.email} | ${p.amount} | ${productNames} | ${message}`;
-            }).join('\n');
-        }
-
-        e.subscribers.forEach(s => {
-            mails.push({
-                sender: ewbMail.noreply(),
-                recipient: s.email,
-                subject: e.name + ' - ' + moment().format('YYYY-MM-DD'),
-                body: eventSummary,
+      let eventSummary = "Inga anmälningar";
+      if (e.payments.length > 0) {
+        eventSummary = "Namn | Epost | Betalt (SEK) | Val | Meddelande\n\n";
+        eventSummary += e.payments
+          .map((p) => {
+            let products = e.addons.filter((a) => {
+              return p.addons.includes(a.product_id);
             });
+
+            let productNames = products
+              .map((product) => {
+                return product.name;
+              })
+              .join(", ");
+
+            let message = p.message === null ? "-" : p.message;
+
+            return `${p.name} | ${p.email} | ${p.amount} | ${productNames} | ${message}`;
+          })
+          .join("\n");
+      }
+
+      e.subscribers.forEach((s) => {
+        mails.push({
+          sender: ewbMail.noreply(),
+          recipient: s.email,
+          subject: e.name + " - " + moment().format("YYYY-MM-DD"),
+          body: eventSummary,
         });
+      });
     });
 
     if (mails.length === 0) {
-        log.info('No messages generated.');
-        process.exit(0);
+      log.info("No messages generated.");
+      process.exit(0);
     }
 
-    return db.task(tx => {
-        let queries = mails.map(m => {
-            return tx.none(`
+    return db.task((tx) => {
+      let queries = mails.map((m) => {
+        return tx.none(
+          `
                 INSERT INTO outgoing_message (sender, recipient, subject, body)
                 VALUES ($[sender], $[recipient], $[subject], $[body])
-            `, m);
-        });
+            `,
+          m
+        );
+      });
 
-        return tx.batch(queries);
+      return tx.batch(queries);
     });
-}).then(messages => {
-    log.info('Queued ' + messages.length + ' message(s).');
+  })
+  .then((messages) => {
+    log.info("Queued " + messages.length + " message(s).");
     process.exit(0);
-}).catch(err => {
+  })
+  .catch((err) => {
     log.error(err);
     process.exit(1);
-});
+  });
