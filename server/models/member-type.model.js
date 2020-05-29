a5d1dfddeeab34a5e0268f44dd90e114260021ec
Dan Johansson <dan.albin.johansson@gmail.com>
2020-05-29 16:27:08 +0200

    Remove unused function

--- server/models/member-type.model.js
+++ server/models/member-type.model.js
@@ -5,9 +5,9 @@
  * @memberOf model
  */
 
-'use strict';
+"use strict";
 
-var db = require('../db').db;
+var db = require("../db").db;
 
 /**
  * Find all member types
@@ -17,7 +17,7 @@ var db = require('../db').db;
  * @returns {Promise<Array|Error>} Resolves to an array of member types
  */
 function index() {
-    return db.any(`
+  return db.any(`
         SELECT id, member_type
         FROM member_type
     `);
@@ -32,31 +32,17 @@ function index() {
  * @returns {Promise<Object|Error>} Resolves to a member type
  */
 function find(memberType) {
-    return db.oneOrNone(`
+  return db.oneOrNone(
+    `
         SELECT id, member_type
         FROM member_type
         WHERE member_type = $1
-    `, memberType);
-}
-
-/**
- * Create a member type
- *
- * @namespace model.MemberType
- * @memberOf model
- * @param {String} memberType - String representation of a member type
- * @returns {Promise<Object|Error>} Resolves to a member type
- */
-function create(memberType) {
-    return db.one(`
-        INSERT INTO member_type (member_type)
-        VALUES ($1)
-        RETURNING id
-    `, memberType);
+    `,
+    memberType
+  );
 }
 
 module.exports = {
-    index: index,
-    find: find,
-    create: create,
+  index: index,
+  find: find,
 };
