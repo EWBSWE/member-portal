7213e4f155d3b469528d806814a5c45f863cb709
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-01 10:26:07 +0200

    Remove Payments view

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,36 +1,37 @@
-'use strict';
+"use strict";
 
-angular.module('ewbMemberApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+angular
+  .module("ewbMemberApp")
+  .controller("NavbarCtrl", function ($scope, $location, Auth) {
     $scope.menu = [
       {
-        title: 'Payments',
-        link: '/payments',
-      }, {
-        title: 'Members',
-        link: '/members',
-      }, {
-        title: 'Events',
-        link: '/events',
-      }, {
-        title: 'Admin',
-        link: '/admin',
-      }, {
-        title: 'Statistics',
-        link: '/statistics'
-      }
+        title: "Members",
+        link: "/members",
+      },
+      {
+        title: "Events",
+        link: "/events",
+      },
+      {
+        title: "Admin",
+        link: "/admin",
+      },
+      {
+        title: "Statistics",
+        link: "/statistics",
+      },
     ];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
+    $scope.logout = function () {
       Auth.logout();
-      $location.path('/login');
+      $location.path("/login");
     };
 
-    $scope.isActive = function(route) {
+    $scope.isActive = function (route) {
       return route === $location.path();
     };
   });
