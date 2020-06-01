4600a79049ac0f0edd1ce3252832d1ee105e3b45
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-01 10:48:47 +0200

    Remove old member columns

--- server/api/member/member.controller.js
+++ server/api/member/member.controller.js
@@ -5,14 +5,14 @@
  * @memberOf controller
  */
 
-'use strict';
+"use strict";
 
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const Member = require('../../models/member.model');
-const Payment = require('../../models/payment.model');
-const OutgoingMessage = require('../../models/outgoing-message.model');
-const ewbMail = require('../../components/ewb-mail');
+const Member = require("../../models/member.model");
+const Payment = require("../../models/payment.model");
+const OutgoingMessage = require("../../models/outgoing-message.model");
+const ewbMail = require("../../components/ewb-mail");
 
 /**
  * Returns all members.
@@ -24,92 +24,99 @@ const ewbMail = require('../../components/ewb-mail');
  * @param {object} next - Express next error function
  */
 function index(req, res, next) {
-    if (req.query.role) {
-        Member.findBy({ role: req.query.role }).then(data => {
-            res.status(200).json(data);
-        }).catch(err => {
-            next(err);
-        });
-    } else {
-        Member.index().then(data => {
-            res.status(200).json(data);
-        }).catch(err => {
-            next(err);
-        });
-    }
-};
+  Member.index()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
 function get(req, res, next) {
-    if (!Number.isInteger(parseInt(req.params.id))) {
-        let badRequest = new Error('Bad request.');
-        badRequest.status = 400;
-        return next(badRequest);
-    }
+  if (!Number.isInteger(parseInt(req.params.id))) {
+    let badRequest = new Error("Bad request.");
+    badRequest.status = 400;
+    return next(badRequest);
+  }
 
-    Member.get(req.params.id).then(data => {
-        if (!data) {
-            return res.sendStatus(404);
-        }
-        res.status(200).json(data);
-    }).catch(err => {
-        next(err);
+  Member.get(req.params.id)
+    .then((data) => {
+      if (!data) {
+        return res.sendStatus(404);
+      }
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      next(err);
     });
-};
+}
 
 function getPayments(req, res, next) {
-    Payment.findBy({ memberId: req.params.id }).then(payments => {
-        return res.status(200).json(payments);
-    }).catch(err => {
-        next(err);
+  Payment.findBy({ memberId: req.params.id })
+    .then((payments) => {
+      return res.status(200).json(payments);
+    })
+    .catch((err) => {
+      next(err);
     });
-};
+}
 
 function create(req, res, next) {
-    if (!req.body.email) {
-        let badRequest = new Error('Bad request.');
-        badRequest.status = 400;
-        return next(badRequest);
-    }
+  if (!req.body.email) {
+    let badRequest = new Error("Bad request.");
+    badRequest.status = 400;
+    return next(badRequest);
+  }
 
-    if (req.body.role) {
-        const error = new Error("Create user is removed");
-        return next(error);
-    } else {
-        Member.findBy({email: req.body.email}).then(members => {
-            if (members.length > 0) {
-                let existingMemberError = new Error('Member exists');
-                existingMemberError.status = 400;
-                return Promise.reject(existingMemberError);
-            }
-
-            return Member.create(req.body);
-        }).then(data => {
-            res.status(201).json(data);
-        }).catch(err => {
-            next(err);
-        });
-    }
-};
-
-function destroy(req, res, next) {
-    Member.get(req.params.id).then(member => {
-        if ((member.role === 'admin' || member.role === 'user') && req.user.role !== 'admin') {
-            let forbidden = new Error('Forbidden');
-            forbidden.status = 403;
-            return Promise.reject(forbidden);
+  if (req.body.role) {
+    const error = new Error("Create user is removed");
+    return next(error);
+  } else {
+    Member.findBy({ email: req.body.email })
+      .then((members) => {
+        if (members.length > 0) {
+          let existingMemberError = new Error("Member exists");
+          existingMemberError.status = 400;
+          return Promise.reject(existingMemberError);
         }
 
-        return Member.destroy(req.params.id);
-    }).then(() => {
-        res.sendStatus(204);
-    }).catch(err => {
+        return Member.create(req.body);
+      })
+      .then((data) => {
+        res.status(201).json(data);
+      })
+      .catch((err) => {
         next(err);
+      });
+  }
+}
+
+function destroy(req, res, next) {
+  Member.get(req.params.id)
+    .then((member) => {
+      if (
+        (member.role === "admin" || member.role === "user") &&
+        req.user.role !== "admin"
+      ) {
+        let forbidden = new Error("Forbidden");
+        forbidden.status = 403;
+        return Promise.reject(forbidden);
+      }
+
+      return Member.destroy(req.params.id);
+    })
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      next(err);
     });
-};
+}
 
 function authCallback(req, res, next) {
-    res.redirect('/')
-};
+  res.redirect("/");
+}
 
 module.exports = {
   index,
--- server/models/member.model.js
+++ server/models/member.model.js
@@ -5,177 +5,192 @@
  * @memberOf model
  */
 
-'use strict';
+"use strict";
 
-var Promise = require('bluebird');
-var crypto = require('crypto');
-var moment = require('moment');
+var Promise = require("bluebird");
+var crypto = require("crypto");
+var moment = require("moment");
 
-var config = require('../config/environment');
+var config = require("../config/environment");
 
-var db = require('../db').db;
-var postgresHelper = require('../helpers/postgres.helper');
+var db = require("../db").db;
+var postgresHelper = require("../helpers/postgres.helper");
 
-const { createPassword, authenticate } = require('../user/PasswordService');
+const { createPassword, authenticate } = require("../user/PasswordService");
 
 const COLUMN_MAP = {
-    email: 'email',
-    hashedPassword: 'hashed_password',
-    salt: 'salt',
-    role: 'role',
-    resetValidity: 'reset_validity',
-    resetToken: 'reset_token',
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    name: 'name',
-    location: 'location',
-    education: 'education',
-    profession: 'profession',
-    memberTypeId: 'member_type_id',
-    gender: 'gender',
-    yearOfBirth: 'year_of_birth',
-    expirationDate: 'expiration_date',
-    chapterId: 'chapter_id',
-    employer: 'employer',
+  email: "email",
+  createdAt: "created_at",
+  updatedAt: "updated_at",
+  name: "name",
+  location: "location",
+  education: "education",
+  profession: "profession",
+  memberTypeId: "member_type_id",
+  gender: "gender",
+  yearOfBirth: "year_of_birth",
+  expirationDate: "expiration_date",
+  chapterId: "chapter_id",
+  employer: "employer",
 };
 
 function create(data) {
-    let dataValid = false;
-    if (Array.isArray(data)) {
-        dataValid = data.map(validate).reduce((a, b) => { return a && b; }, true);
-    } else if (typeof data === 'object'){
-        dataValid = validate(data);
+  let dataValid = false;
+  if (Array.isArray(data)) {
+    dataValid = data.map(validate).reduce((a, b) => {
+      return a && b;
+    }, true);
+  } else if (typeof data === "object") {
+    dataValid = validate(data);
+  }
+
+  if (!dataValid) {
+    return Promise.reject("Invalid member");
+  }
+
+  // Helper function to create member
+  let _create = (member, transaction) => {
+    if (member.password !== undefined && member.password !== null) {
+      const pw = createPassword(member.password);
+
+      Object.assign(member, {
+        hashedPassword: pw.hashed,
+        salt: pw.salt,
+        resetValidity: null,
+        resetToken: null,
+      });
+
+      delete member.password;
     }
 
-    if (!dataValid) {
-        return Promise.reject('Invalid member');
-    }
+    let { columns, wrapped } = postgresHelper.insert(COLUMN_MAP, member);
 
-    // Helper function to create member
-    let _create = (member, transaction) => {
-        if (member.password !== undefined && member.password !== null) {
-            const pw = createPassword(member.password);
-
-            Object.assign(member, {
-                hashedPassword: pw.hashed,
-                salt: pw.salt,
-                resetValidity: null,
-                resetToken: null,
-            });
-
-            delete member.password;
-        }
-
-        let {columns, wrapped} = postgresHelper.insert(COLUMN_MAP, member);
-
-        let sql = `
+    let sql = `
             INSERT INTO member (${columns})
             VALUES (${wrapped})
             RETURNING id, email, expiration_date
         `;
 
-        return transaction.one(sql, member);
-    };
+    return transaction.one(sql, member);
+  };
 
-    // If data is an array, assume we want to create multiple members.
-    // Otherwise we try to create a single member.
-    if (Array.isArray(data)) {
-        return db.tx(transaction => {
-            let queries = data.map(member => {
-                return _create(member, transaction);
-            });
+  // If data is an array, assume we want to create multiple members.
+  // Otherwise we try to create a single member.
+  if (Array.isArray(data)) {
+    return db.tx((transaction) => {
+      let queries = data.map((member) => {
+        return _create(member, transaction);
+      });
 
-            return transaction.batch(queries);
-        });
-    } else {
-        return _create(data, db);
-    }
+      return transaction.batch(queries);
+    });
+  } else {
+    return _create(data, db);
+  }
 }
 
 function validate(member) {
-    if (!validEmail(member.email)) {
-        return false;
-    }
+  if (!validEmail(member.email)) {
+    return false;
+  }
 
-    // Password is optional
-    if (member.password && !validPassword(member.password)) {
-        return false;
-    }
-    // Role is optional
-    if (member.role && !validRole(member.role)) {
-        return false;
-    }
+  // Password is optional
+  if (member.password && !validPassword(member.password)) {
+    return false;
+  }
+  // Role is optional
+  if (member.role && !validRole(member.role)) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 /**
  * Updates a member
- * 
+ *
  * @memberOf model.Member
  * @param {number} id - The member id
  * @param {object} data - The attributes that should be updated
  * @returns {Promise<object,Error>} Resolves to the updated member
  */
 function update(id, data) {
-    return new Promise((resolve, reject) => {
-        if (data.newPassword !== undefined && data.newPassword !== null && data.password !== undefined && data.password !== null) {
-            db.one('SELECT hashed_password, salt FROM member WHERE id = $1', id).then(member => {
-                if (!authenticate(data.password, member.hashed_password, member.salt)) {
-                    return Promise.reject('Invalid password');
-                }
+  return new Promise((resolve, reject) => {
+    if (
+      data.newPassword !== undefined &&
+      data.newPassword !== null &&
+      data.password !== undefined &&
+      data.password !== null
+    ) {
+      db.one("SELECT hashed_password, salt FROM member WHERE id = $1", id)
+        .then((member) => {
+          if (
+            !authenticate(data.password, member.hashed_password, member.salt)
+          ) {
+            return Promise.reject("Invalid password");
+          }
 
-                return Promise.resolve();
-            }).then(() => {
-                if (!validPassword(data.newPassword)) {
-                    return Promise.reject('Invalid password');
-                }
+          return Promise.resolve();
+        })
+        .then(() => {
+          if (!validPassword(data.newPassword)) {
+            return Promise.reject("Invalid password");
+          }
 
-                const pw = createPassword(data.newPassword);
+          const pw = createPassword(data.newPassword);
 
-                Object.assign(data, {
-                    hashedPassword: pw.hashed,
-                    salt: pw.salt,
-                    resetValidity: null,
-                    resetToken: null,
-                });
+          Object.assign(data, {
+            hashedPassword: pw.hashed,
+            salt: pw.salt,
+            resetValidity: null,
+            resetToken: null,
+          });
 
-                resolve();
-            }).catch(err => {
-                reject(err);
-            });
-        } else if (data.newPassword !== undefined && data.newPassword !== null && data.resetToken !== undefined && data.resetToken !== null) {
-            if (!validPassword(data.newPassword)) {
-                return Promise.reject('Invalid password');
-            }
+          resolve();
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    } else if (
+      data.newPassword !== undefined &&
+      data.newPassword !== null &&
+      data.resetToken !== undefined &&
+      data.resetToken !== null
+    ) {
+      if (!validPassword(data.newPassword)) {
+        return Promise.reject("Invalid password");
+      }
 
-            const pw = createPassword(data.newPassword);
+      const pw = createPassword(data.newPassword);
 
-            Object.assign(data, {
-                hashedPassword: pw.hashed,
-                salt: pw.salt,
-                resetValidity: null,
-                resetToken: null,
-            });
+      Object.assign(data, {
+        hashedPassword: pw.hashed,
+        salt: pw.salt,
+        resetValidity: null,
+        resetToken: null,
+      });
 
-            resolve();
-        } else {
-            resolve();
-        }
-    }).then(() => {
-        let mapped = postgresHelper.update(COLUMN_MAP, data);
+      resolve();
+    } else {
+      resolve();
+    }
+  }).then(() => {
+    let mapped = postgresHelper.update(COLUMN_MAP, data);
 
-        if (mapped === null) {
-            return Promise.reject('No attributes to update');
-        }
+    if (mapped === null) {
+      return Promise.reject("No attributes to update");
+    }
 
-        return db.one(`
+    return db.one(
+      `
             UPDATE member
             SET ${mapped}
             WHERE id = $[id]
             RETURNING *
-        `, Object.assign(data, {id: id}));
-    });
+        `,
+      Object.assign(data, { id: id })
+    );
+  });
 }
 
 /**
@@ -185,7 +200,7 @@ function update(id, data) {
  * @returns {Promise}
  */
 function index() {
-    return db.any(`
+  return db.any(`
         SELECT
             member.id,
             email,
@@ -206,7 +221,7 @@ function index() {
 }
 
 function activeMembers() {
-    return db.any(`
+  return db.any(`
         SELECT
             member.id,
             email,
@@ -229,12 +244,12 @@ function activeMembers() {
 }
 
 function get(id) {
-    return db.oneOrNone(`
+  return db.oneOrNone(
+    `
         SELECT
             member.id,
             email,
             name,
-            role,
             location,
             education,
             profession,
@@ -243,17 +258,19 @@ function get(id) {
             gender,
             year_of_birth,
             expiration_date,
-            reset_token,
             chapter_id,
             employer
         FROM member
         LEFT JOIN member_type ON (member.member_type_id = member_type.id)
         WHERE member.id = $1
-    `, id);
+    `,
+    id
+  );
 }
 
 function find(email) {
-    return db.oneOrNone(`
+  return db.oneOrNone(
+    `
         SELECT
             id,
             email,
@@ -267,79 +284,72 @@ function find(email) {
             expiration_date
         FROM member
         WHERE email = $1
-    `, email);
+    `,
+    email
+  );
 }
 
 function destroy(id) {
-    return db.none(`DELETE FROM member WHERE id = $1`, id);
+  return db.none(`DELETE FROM member WHERE id = $1`, id);
 }
 
 function validEmail(email) {
-    if (!email) {
-        return false;
-    }
+  if (!email) {
+    return false;
+  }
 
-    var re = /.+@.+/i;
+  var re = /.+@.+/i;
 
-    return re.test(email);
+  return re.test(email);
 }
 
 function validRole(role) {
-    return role && config.userRoles.indexOf(role) > -1;
+  return role && config.userRoles.indexOf(role) > -1;
 }
 
 function validPassword(password) {
-    return password && password.length >= 8;
-}
-
-function createResetToken(id) {
-    let resetToken = crypto.randomBytes(24).toString('hex');
-
-    return db.one(`
-        UPDATE member
-        SET
-            reset_validity = NOW() + '15 minutes'::interval,
-            reset_token = $2
-        WHERE id = $1
-        RETURNING email, reset_token
-    `, [id, resetToken]);
+  return password && password.length >= 8;
 }
 
 function findBy(data) {
-    let wheres = postgresHelper.where(COLUMN_MAP, data);
+  let wheres = postgresHelper.where(COLUMN_MAP, data);
 
-    return db.any(`
-        SELECT id, email, role, reset_validity, expiration_date
+  return db.any(
+    `
+        SELECT id, email
         FROM member
         WHERE ${wheres.clause}
-    `, wheres.data);
+    `,
+    wheres.data
+  );
 }
 
 function extendMembership(member, product) {
-    if (!member.id) {
-        throw 'Missing id';
-    }
+  if (!member.id) {
+    throw "Missing id";
+  }
 
-    let expirationDate = moment().add(product.attribute.days, 'days').toDate();
+  let expirationDate = moment().add(product.attribute.days, "days").toDate();
 
-    if (member.expiration_date !== null) {
-        expirationDate = moment(member.expiration_date).add(product.attribute.days, 'days').toDate();
-    }
+  if (member.expiration_date !== null) {
+    expirationDate = moment(member.expiration_date)
+      .add(product.attribute.days, "days")
+      .toDate();
+  }
 
-    return update(member.id, {expirationDate: expirationDate});
+  return update(member.id, { expirationDate: expirationDate });
 }
 
 module.exports = {
-    index: index,
-    activeMembers: activeMembers,
-    get: get,
-    create: create,
-    update: update,
-    authenticate: authenticate,
-    find: find,
-    findBy: findBy,
-    destroy: destroy,
-    createResetToken: createResetToken,
-    validate: validate,
-    extendMembership: extendMembership,
-}
+  index: index,
+  activeMembers: activeMembers,
+  get: get,
+  create: create,
+  update: update,
+  authenticate: authenticate,
+  find: find,
+  findBy: findBy,
+  destroy: destroy,
+  validate: validate,
+  extendMembership: extendMembership,
+};
