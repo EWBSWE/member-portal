63f977ea8958bdceff4cb1ff8b1bc7531efc63ce
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-02 10:24:08 +0200

    Fix broken path in test

--- server/test/RouteBuilderTest.js
+++ server/test/RouteBuilderTest.js
@@ -1,47 +1,45 @@
-'use strict';
+"use strict";
 
-const sinon = require('sinon');
+const sinon = require("sinon");
 
-const RouteBuilder = require('../RouteBuilder');
+const RouteBuilder = require("../src/RouteBuilder");
 
-describe('RouteBuilder', function() {
+describe("RouteBuilder", function () {
   const successEndpoint = () => ({});
-  const errorEndpoint = () => { throw new Error('This is an error'); };
+  const errorEndpoint = () => {
+    throw new Error("This is an error");
+  };
 
   let sandbox;
-  beforeEach(function() {
+  beforeEach(function () {
     sandbox = sinon.createSandbox();
   });
-  afterEach(function() {
+  afterEach(function () {
     sandbox.restore();
   });
 
-  describe('Error handling', function() {
-    it('should throw error if no endpoint', function() {
+  describe("Error handling", function () {
+    it("should throw error if no endpoint", function () {
       assert.throws(() => new RouteBuilder().build(), /Missing endpoint/);
     });
 
-    it('should pass error to handler if missing require params', async function() {
+    it("should pass error to handler if missing require params", async function () {
       const route = new RouteBuilder(successEndpoint)
-	    .requiredParams(['foo'])
-	    .build();
+        .requiredParams(["foo"])
+        .build();
 
-      await route(
-	{ body: {} },
-	{},
-	e => assert.match(e.message, /Missing parameters/)
+      await route({ body: {} }, {}, (e) =>
+        assert.match(e.message, /Missing parameters/)
       );
     });
 
-    it('should pass error to handler if endpoint throws', async function() {
+    it("should pass error to handler if endpoint throws", async function () {
       const route = new RouteBuilder(errorEndpoint)
-	    .requiredParams(['foo'])
-	    .build();
+        .requiredParams(["foo"])
+        .build();
 
-      await route(
-	{ body: { foo: 'bar' } },
-	{},
-	e => assert.match(e.message, /This is an error/)
+      await route({ body: { foo: "bar" } }, {}, (e) =>
+        assert.match(e.message, /This is an error/)
       );
     });
   });
