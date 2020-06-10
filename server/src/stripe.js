3d5d95b91308663c2de276826efef8c541f8825c
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-10 09:56:15 +0200

    Remove unused Stripe function

--- server/src/stripe.js
+++ server/src/stripe.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const STRIPE_KEY = process.env.STRIPE_KEY;
 const STRIPE_CHECKOUT_KEY = process.env.STRIPE_CHECKOUT_KEY;
@@ -10,46 +10,33 @@ if (!STRIPE_CHECKOUT_KEY) {
   throw new Error(`Env variable STRIPE_CHECKOUT_KEY is missing.`);
 }
 
-const stripe = require('stripe')(STRIPE_KEY);
+const stripe = require("stripe")(STRIPE_KEY);
 
 function getCheckoutKey() {
   return STRIPE_CHECKOUT_KEY;
 }
 
-function processCharge(chargeAttributes, stripeToken, successCallback, errorCallback) {
-    stripe.charges.create({
-        currency: chargeAttributes.currency,
-        amount: chargeAttributes.amount * 100,
-        source: stripeToken.id,
-        description: chargeAttributes.description,
-    }, function(err, charge) {
-        if (err === null) {
-            successCallback(charge);
-        } else {
-            errorCallback(err);
-        }
-    });
-}
-
 async function processCharge2(stripeToken, currency, amount, description) {
   return new Promise((resolve, reject) => {
-    stripe.charges.create({
-      source: stripeToken.id,
-      amount: amount * 100,
-      currency,
-      description
-    }, function(err, charge) {
-      if (err) {
-	reject(err);
-      } else {
-	resolve(charge);
+    stripe.charges.create(
+      {
+        source: stripeToken.id,
+        amount: amount * 100,
+        currency,
+        description,
+      },
+      function (err, charge) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(charge);
+        }
       }
-    });
+    );
   });
 }
 
 module.exports = {
   getCheckoutKey,
-  processCharge,
-  processCharge2
+  processCharge2,
 };
