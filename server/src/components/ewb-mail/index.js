08798c7a704723d2b374cd2199a0d595dea69b56
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-10 09:54:33 +0200

    Change email sender name

--- server/src/components/ewb-mail/index.js
+++ server/src/components/ewb-mail/index.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 module.exports = {
-    noreply: function() {
-        return 'noreply@ewb-swe.org';
-    },
-    sender: function() {
-        // Should perhaps be set from a config file instead of directly in the
-        // code like this.
-        return 'Ingenjörer utan gränser <info@ewb-swe.org>';
-    }
+  noreply: function () {
+    return "noreply@ewb-swe.org";
+  },
+  sender: function () {
+    // Should perhaps be set from a config file instead of directly in the
+    // code like this.
+    return "Engineers Without Borders Sweden <info@ewb-swe.org>";
+  },
 };
