e3c9735289217da120df09d107b760cf798eebf1
Dan Johansson <dan.albin.johansson@gmail.com>
2020-06-16 13:52:44 +0200

    Return 404 when resource not found

--- server/src/App.ts
+++ server/src/App.ts
@@ -9,7 +9,6 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../env") });
 console.log("NODE_ENV:", process.env.NODE_ENV);
 const env = deserialize(process.env.NODE_ENV!);
 
-const errors = require("./components/errors");
 const logger = require("./config/logger");
 const favicon = require("serve-favicon");
 const morgan = require("morgan");
@@ -35,7 +34,9 @@ function setupRoutes(app: express.Application) {
   // All undefined asset or api routes should return a 404
   app
     .route("/:url(api|auth|components|app|bower_components|assets)/*")
-    .get(errors[404]);
+    .get(function (req: express.Request, res: express.Response) {
+      res.sendStatus(404);
+    });
 
   // All other routes should redirect to the index.html
   app.route("/*").get(function (req: express.Request, res: express.Response) {
@@ -56,7 +57,7 @@ function setupErrorHandler(app: express.Application) {
       err.message = req.url + "\n" + err.message;
       logger.error(err);
 
-      res.status(500);
+      res.sendStatus(500);
     });
   } else if (env === AppEnv.DEVELOPMENT) {
     app.use(function (
